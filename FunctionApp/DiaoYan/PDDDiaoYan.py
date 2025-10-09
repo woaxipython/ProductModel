ff4eb7bcf05deff7b7b80c0402a4f8f35d24a24f
Your Name <you@example.com>
2025-10-09 18:47:38 +0800

    change

---
+++ FunctionApp/DiaoYan/PDDDiaoYan.py
@@ -0,0 +1,14 @@
+class PDDDiaoYan:
+    """
+    对于拼多多的调研，主要观察在淘宝销售良好的品牌，在拼多多是否继续销售良好
+    了解该品牌在拼多多的销售情况。主管人员务必自行在拼多多进行关键词搜索，以加深第一印象。
+    现场有神灵
+    在拼多多的主要区间售价、各个榜单的销量、评价数量、评价等级分布等。
+    """
+    def MarketAnalysis(self, name):
+        print('{} 进行了调研'.format(name))
+        return name
+    def BrandAnalysis(self, name):
+        # 对在拼多多的品牌进行分析，观察其产品与在天猫平台的区别是什么
+        print('{} 进行了调研'.format(name))
+        return name
